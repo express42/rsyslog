d5e6ed01d40b1c125f17926b2be14da3bf3bd3ed
Igor Kurochkin <ikurochkin@express42.com>
2014-12-03 18:18:22 +0300

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,7 @@ maintainer_email 'info@express42.com'
 license          'MIT'
 description      'Installs and configures rsyslog'
 version          '0.0.5'
+
+recipe            'rssylog::default', 'Installs and configures rsyslog'
+
+supports          'ubuntu'
