2cffcb797229643f0d78548c3a9adde2a9385bff
Igor Kurochkin <ikurochkin@express42.com>
2014-12-10 19:41:37 +0300

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'rsyslog'
 maintainer       'LLC Express 42'
-maintainer_email 'info@express42.com'
+maintainer_email 'cookbooks@express42.com'
 license          'MIT'
 description      'Installs and configures rsyslog. Provides LWRP for creating rules.'
 version          '0.0.5'
 
-recipe            'rsyslog::default', 'Installs and configures rsyslog'
+recipe           'rsyslog::default', 'Installs and configures rsyslog.'
 
-supports          'ubuntu'
+supports         'ubuntu'
